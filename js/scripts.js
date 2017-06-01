e92b9ef79d5291eaa5b6b17e5eb96d7b1a1ef826
Calla Rudolph and Steve Spitz <calla@>
2017-06-01 15:15:40 -0700

    add form submissions; add submission listeners; add conditions for equilateral triangle

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  $("#everything").submit(function(event){
+    event.preventDefault();
+console.log("test")
+
+    var triside1 = parseInt($("input#side1").val());
+    var triside2 = parseInt($("input#side2").val());
+    var triside3 = parseInt($("input#side3").val());
+    console.log(triside1)
+    console.log(triside2)
+    console.log(triside3)
+
+    var equilateral = "Your triangle is equilateral"
+
+    if (triside1 === triside2 && triside1 === triside3) {
+      $("#answer").text(equilateral);
+      $("#answer").show();
+    }
+
+
+  });
+});
